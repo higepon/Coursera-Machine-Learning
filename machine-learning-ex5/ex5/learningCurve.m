fd72d5c83cfe5a9077fc785446145264947f750d
Taro Minowa Higepon <higepon@gmail.com>
2015-12-31 06:33:17 -0800

    ex5 learning curve.

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,8 +53,15 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
+for i = 1:m
+    xi = X(1:i, :);
+    yi = y(1:i);
+    [theta] = trainLinearReg(xi, yi, lambda);
+    [J, grad] = linearRegCostFunction(xi, yi, theta, 0);
+    error_train(i) = J;
+    [J, grad] = linearRegCostFunction(Xval, yval, theta, 0);
+    error_val(i) = J;
+end;
 
 
 
