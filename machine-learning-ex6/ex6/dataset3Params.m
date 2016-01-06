ea9b74d865bbe17e54a56c9828e254656cfb2134
Taro Minowa Higepon <higepon@gmail.com>
2016-01-06 05:44:39 -0800

    Best C/sigma.

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,12 +23,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+## C_vec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+## sigma_vec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+## best_C = 0;
+## best_sigma = 0;
+## min_error = 9999999;
 
+## for i = 1:length(C_vec)
+##   C = C_vec(i);
+##   for j = 1:length(sigma_vec)
+##     sigma = sigma_vec(j);
+##     model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+##     predictions = svmPredict(model, Xval);
+##     error = mean(double(predictions ~= yval));
+##     if error < min_error
+##       min_error = error;
+##       best_C = C;
+##       best_sigma = sigma;
+##     end
+##   end
+## end
 
+## C = best_C
+## sigma = best_sigma
 
-
-
+C =  1
+sigma =  0.10000
 % =========================================================================
 
 end
